febad755f36d2a428731bc395e1543b5d44e2d7a
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-09-21 12:44:40 +0200

    feat: clase Principal para ejecutar la simulacion de 4h

---
+++ entregas/aguilarAdriana/Cola/Principal.java
@@ -0,0 +1,9 @@
+package entregas.aguilarAdriana.Cola;
+
+public class Principal {
+
+    public static void main(String[] args) {
+        SimulacionEscenario escenario = new SimulacionEscenario();
+        escenario.ejecutar();
+    }
+}
