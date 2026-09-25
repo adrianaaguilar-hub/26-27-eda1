553ae50f136138440d88cdbe1636afcbccec38b7
adrianaaguilar-hub <adriana.aguilar@alumnos.uneatlantico.es>
2026-09-25 10:26:17 +0200

    refactor(CentroComercial): agregar logica de LlegadaCliente sin Cola

--- entregas/aguilarAdriana/SupermercadoSinCola/CentroComercial.java
+++ entregas/aguilarAdriana/SupermercadoSinCola/CentroComercial.java
@@ -2,16 +2,17 @@ package entregas.aguilarAdriana.Cola;
 
 public class CentroComercial {
 
-    private Cola cola;
+    private Cliente primerCliente;
     private Caja[] cajas;
     private Tiempo tiempo;
+    private int totalClientesHistorico; 
+    private int minutosSinClientes;
     private boolean llegaClienteEsteMinuto;
     final private double PROBABILIDAD_LLEGADA = 0.4;
     private Console console;
 
     public CentroComercial() {
         console = new Console();
-        cola = new Cola();
         cajas = new Caja[5];
         for (int i = 0; i < cajas.length; i++) {
             cajas[i] = new Caja(i + 1);
@@ -19,13 +20,16 @@ public class CentroComercial {
         cajas[0] = new CajaExpress(1);
         cajas[4] = new CajaExpress(5);
         tiempo = new Tiempo();
+        primerCliente = null;
+        totalClientesHistorico = 0;
+        minutosSinClientes = 0;
     }
 
     public void simular() {
         do {
             tiempo.avanzar();
             this.procesarLlegadaCliente();
-            cola.registrarEstado();
+            this.registrarEstadoVirtual();
             this.asignarClientesACajas();
             this.procesarAtencionCajas();
             this.mostrarEstado();
@@ -34,73 +38,79 @@ public class CentroComercial {
         this.mostrarResumen();
     }
 
-    private void mostrarResumen() {
-        int minutosSinClientes = cola.obtenerMinutosSinClientes();
-        int personasEnCola = cola.obtenerCantidadPersonasEnCola();
-        int personasAtendidas = this.obtenerPersonasAtendidas();
-        int itemsVendidos = this.obtenerItemsVendidos();
+    private void procesarLlegadaCliente() {
+        llegaClienteEsteMinuto = Math.random() <= PROBABILIDAD_LLEGADA;
+        if (llegaClienteEsteMinuto) {
+            totalClientesHistorico++;
+            boolean esPrioritario = Math.random() <= 0.2; 
+            Cliente nuevoCliente = new Cliente(totalClientesHistorico, esPrioritario);
 
-        console.writeln("Personas atendidas: " + personasAtendidas);
-        console.writeln("Personas en cola al cierre: " + personasEnCola);
-        console.writeln("Items vendidos: " + itemsVendidos);
-        console.writeln("Minutos sin clientes en cola: " + minutosSinClientes);
-    }
-
-    private int obtenerPersonasAtendidas(){
-        int totalItems=0;
-        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
-            totalItems= totalItems + cajas[numeroCaja].obtenerPersonasAtendidas();
+            if (primerCliente == null) {
+                primerCliente = nuevoCliente;
+            } else {
+                if (nuevoCliente.tienePrioridad() && !primerCliente.tienePrioridad()) {
+                    nuevoCliente.enlazarSiguiente(primerCliente);
+                    primerCliente = nuevoCliente;
+                } else {
+                    primerCliente.formarse(nuevoCliente);
+                }
+            }
         }
-        return totalItems;
     }
 
-    private int obtenerItemsVendidos(){
-        int totalItems=0;
-        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
-            totalItems = totalItems + cajas[numeroCaja].obtenerItemsVendidos();
+    private void asignarClientesACajas() {
+        for (int i = 0; i < cajas.length; i++) {
+            if (cajas[i].estaLibre() && primerCliente != null && cajas[i].puedeAtender(primerCliente)) {
+                cajas[i].asignar(primerCliente);
+                primerCliente = primerCliente.obtenerSiguiente(); 
+            }
         }
-        return totalItems;
     }
 
-    private void pausar() {
-        console.pause(1);
+    private void registrarEstadoVirtual() {
+        if (primerCliente == null) {
+            minutosSinClientes++;
+        }
     }
 
     private void mostrarEstado() {
         console.cleanScreen();
         tiempo.mostrar(llegaClienteEsteMinuto);
-        cola.mostrar();
-        this.mostrarCajas();
-    }
-
-    private void mostrarCajas(){
-        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
-            cajas[numeroCaja].mostrar();
+        
+        Cliente actual = primerCliente;
+        while (actual != null) {
+            actual.mostrar();
+            actual = actual.obtenerSiguiente();
+        }
+        console.writeln("\n");
+        
+        for (int i = 0; i < cajas.length; i++) {
+            cajas[i].mostrar();
         }
     }
 
-    private void procesarAtencionCajas() {
-        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
-            cajas[numeroCaja].avanzarAtencion();
+    private void mostrarResumen() {
+        int personasAtendidas = 0;
+        int itemsVendidos = 0;
+        for (int i = 0; i < cajas.length; i++) {
+            personasAtendidas += cajas[i].obtenerPersonasAtendidas();
+            itemsVendidos += cajas[i].obtenerItemsVendidos();
         }
+        
+        int personasEnEspera = 0;
+        Cliente actual = primerCliente;
+        while (actual != null) {
+            personasEnEspera++;
+            actual = actual.obtenerSiguiente();
+        }
+
+        console.writeln("Personas atendidas: " + personasAtendidas);
+        console.writeln("Personas sin atender al cierre: " + personasEnEspera);
+        console.writeln("Items vendidos: " + itemsVendidos);
+        console.writeln("Minutos sin clientes esperando: " + minutosSinClientes);
     }
 
-    private void asignarClientesACajas() {
-        for(int numeroCaja=0; numeroCaja<cajas.length; numeroCaja++){
-            if (cajas[numeroCaja].estaLibre() 
-                && cola.hayClientes()
-                && cajas[numeroCaja].puedeAtender(cola.primero())){
-                Cliente cliente = cola.quitarCliente();
-                cajas[numeroCaja].asignar(cliente);
-            }
-        }
-    }
-
-    private void procesarLlegadaCliente() {
-        llegaClienteEsteMinuto = Math.random() <= PROBABILIDAD_LLEGADA;
-        if (llegaClienteEsteMinuto) {
-            Cliente cliente = new Cliente();
-            cola.añadirCliente(cliente);
-        }
+    private void pausar() {
+        console.pause(1);
     }
 }
